a7a01569fe40ec81116400eb62cccbf485afd3fd
kagamma <kagamma.km@gmail.com>
2021-02-22 20:32:41 +0700

    TCastle2DParticleEffect from class -> advanced record, this will break compatibility

--- demo/basic/game.pas
+++ demo/basic/game.pas
@@ -44,7 +44,7 @@ begin
   Emitter.EmissionTime := 0;
   Emitter.ReleaseWhenDone := true;
   Emitter := TCastle2DParticleEmitter.Create(T);
-  Emitter.LoadEffect(AEffect, false);
+  Emitter.LoadEffect(AEffect);
   Emitter.StartEmitting := True;
   T.Add(Emitter);
 end;
@@ -100,10 +100,6 @@ begin
   MenuScene.ProcessEvents := true;
   SceneManager.Items.Add(MenuScene);
 
-  EffectFire := TCastle2DParticleEffect.Create;
-  EffectTrippy := TCastle2DParticleEffect.Create;
-  EffectJellyFish := TCastle2DParticleEffect.Create;
-  EffectSpiral := TCastle2DParticleEffect.Create;
   EffectFire.Load(ApplicationData('fire.pex'));
   EffectTrippy.Load(ApplicationData('trippy.pex'));
   EffectJellyFish.Load(ApplicationData('jellyfish.pex'));
@@ -113,7 +109,7 @@ begin
   T.Translation := Vector3(-213, 0, 0);
   T.Scale := Vector3(1, -1, 1);
   Emitter := TCastle2DParticleEmitter.Create(T);
-  Emitter.LoadEffect(EffectFire, false);
+  Emitter.LoadEffect(EffectFire);
   Emitter.StartEmitting := True;
   T.Add(Emitter);
 
